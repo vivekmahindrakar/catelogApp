4f9d1c4286c99b67f0f1d2f55ba153917bea3554
vivekmahindrakar <vivekmahindrakar08@gmail.com>
2022-12-01 20:02:04 +0530

    day 15 completed => Beautiful ui through Velocity_x learnt

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
+//import 'dart:html';
 
+import "package:velocity_x/velocity_x.dart";
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 import 'package:flutter_application_1/models/catelog.dart';
+import 'package:flutter_application_1/utils/themes/theme.dart';
 import 'package:flutter_application_1/widget/drawer.dart';
 
 import '../widget/item_widget.dart';
@@ -35,54 +40,111 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Catelog App",
+        body: SafeArea(
+      child: Container(
+        padding: Vx.m24,
+        margin: EdgeInsets.only(),
+        decoration: BoxDecoration(color: MyTheme.cream),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Header(),
+            SizedBox(
+              height: 20,
+            ),
+            if (Catelog.Items.isNotEmpty)
+              CatelogList().expand()
+            else
+              Container(
+                  padding: EdgeInsets.symmetric(vertical: 220, horizontal: 120),
+                  child: Center(child: const CircularProgressIndicator()))
+          ],
         ),
       ),
-      body: Material(
-          child: (Catelog.Items.isNotEmpty)
-              ? GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemBuilder: (context, index) {
-                    var item = Catelog.Items[index];
-                    return Card(
-                      clipBehavior: Clip.antiAlias,
-                      margin: EdgeInsets.all(10),
-                      elevation: 0.3,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      child: GridTile(
-                        child: Image.network(item.image),
-                        header: Container(
-                          decoration: BoxDecoration(
-                              color: Color.fromARGB(255, 241, 253, 255)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Text(item.name),
-                          ),
-                        ),
-                        footer: Container(
-                          decoration: BoxDecoration(
-                              color: Color.fromARGB(255, 1, 72, 82)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Text(
-                              "\$${item.price.toString()}",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                  itemCount: Catelog.Items.length,
-                )
-              : Center(
-                  child: CircularProgressIndicator(),
-                )),
-      drawer: MyDrawer(),
+    ));
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catelog App".text.xl5.bold.color(MyTheme.darkBlue).make(),
+        "Trending Products".text.xl2.bold.color(MyTheme.darkBlue).make(),
+      ],
     );
+  }
+}
+
+class CatelogList extends StatelessWidget {
+  const CatelogList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.only(top: 20),
+      itemCount: Catelog.Items.length,
+      itemBuilder: (context, index) {
+        var catelog = Catelog.Items[index];
+        return CatelogItem(item: catelog);
+      },
+    );
+  }
+}
+
+class CatelogItem extends StatelessWidget {
+  const CatelogItem({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+  final Item item;
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CatelogImage(item: item),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            item.name.text.lg.bold.make(),
+            item.desc.text.textStyle(context.captionStyle).bold.make(),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              buttonPadding: Vx.mH8,
+              children: [
+                "\$${item.price}".text.xl.bold.make(),
+                ElevatedButton(
+                  onPressed: (() {}),
+                  child: "Buy".text.color(Colors.white).make(),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(MyTheme.darkBlue)),
+                )
+              ],
+            )
+          ],
+        ))
+      ],
+    )).rounded.square(150).color(Colors.white).make().p(10);
+  }
+}
+
+class CatelogImage extends StatelessWidget {
+  const CatelogImage({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+  final Item item;
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(item.image).box.rounded.p12.make().w32(context).p(10);
   }
 }
--- lib/utils/themes/theme.dart
+++ lib/utils/themes/theme.dart
@@ -3,12 +3,15 @@ import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
-        primarySwatch: Colors.lightBlue,
-        fontFamily: GoogleFonts.lato().fontFamily,
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.poppins().fontFamily,
         appBarTheme: AppBarTheme(
             centerTitle: true,
             color: Colors.white,
             elevation: 0.0,
             iconTheme: IconThemeData(color: Colors.black)),
       );
+
+  static Color cream = Color(0xfff5f5f5);
+  static Color darkBlue = Color(0xff403b58);
 }
